e464279fd8a823d31bd650d2e46cf691c0bd89d8
Andrian Harry Kenneth <63767615+RedmiAndrian@users.noreply.github.com>
2021-11-28 14:28:05 +0800

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "Font Awesome 5 Free Solid:size=10", "Font Awesome 5 Free Brands:size=10", "Font Awesome 5 Free Regular:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "Font Awesome 5 Free Solid:size=10", "Font Awesome 5 Free Brands:size=12", "Font Awesome 5 Free Regular:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", };
+static const char *tags[] = { "", "", "", "", "", "", };
 
 static const Rule rules[] = {
 	/* xprop(1):
